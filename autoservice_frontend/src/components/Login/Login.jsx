ce84564dee6eb6eb898b6d6fd04e11fd8f236a2c
Vadim Gushchin <gushchinvadim@gmail.com>
2026-07-17 10:31:16 +0300

    Fix Login

--- autoservice_frontend/src/components/Login/Login.jsx
+++ autoservice_frontend/src/components/Login/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { API_BASE_URL } from '../config/config';
 import './Login.css';
 
 function Login() {
