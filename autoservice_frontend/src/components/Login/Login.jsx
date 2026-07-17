2362559070118d8ba41aba21402b2d343c394a14
Vadim Gushchin <gushchinvadim@gmail.com>
2026-07-17 10:44:12 +0300

    Fix import path in Login.jsx and correct API endpoint

--- autoservice_frontend/src/components/Login/Login.jsx
+++ autoservice_frontend/src/components/Login/Login.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { API_BASE_URL } from "../../config/config";
 import './Login.css';
 
 function Login() {
@@ -23,7 +24,7 @@ function Login() {
         setError('');
 
         try {
-            const response = await axios.post(`${API_BASE_URL}/api/token/`, {
+            const response = await axios.post(`${API_BASE_URL}token/`, {
                 username: formData.username,
                 password: formData.password
             });
